3866972e2bf13c62bd8d94e086a4df16fc225f8d
ajmal4414 <ajmalsckl@gmail.com>
2024-09-28 20:10:26 +0530

    First commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,92 @@
+import React,{useState, useEffect} from 'react';
+import './Gallery.css';
+//import logo from '../assets/logo192.png'
+function Gallery(){
+
+const [images, setImages] = useState([]);
+
+useEffect(()=>{
+
+    const fetchImages=()=>{
+        const data=[
+            {
+                id:1,
+                url:"https://picsum.photos/id/1/300/200",
+                title:"Image 1"
+            },
+        
+            {
+                id:2,
+                url:"https://picsum.photos/id/2/300/200",
+                title:"image 2"
+            },
+        
+            {
+                id:3,
+                url:"https://picsum.photos/id/3/300/200",
+                title:"image 3"
+            },
+        
+            {
+                id:4,
+                url:"https://picsum.photos/id/4/300/200",
+                title:"image 4"
+            },
+        
+            {
+                id:5,
+                url:"https://picsum.photos/id/5/300/200",
+                title:"image 5"
+            }
+        ]
+        setImages(data);
+    }
+
+    fetchImages()
+},[])
+
+const handleDeleteImage = (id)=>{
+    setImages(images.filter((image)=> image.id !== id)) 
+
+}
+
+    return(
+        <div>
+            <h1 className="title">Image Gallery App</h1>
+
+
+{
+    images.length>0 ? (
+        
+        
+        <div className="image-grid">
+    
+
+    {
+    
+
+        images.map((image)=>(
+
+            
+        <div className="image-card" key={image.id}>
+        <img src={image.url} alt="image"/>
+        <div className="image-details">
+            <h3>{image.title}</h3>
+            <button onClick={()=>handleDeleteImage(image.id)}>Delete</button>
+        </div>
+    </div>
+    
+        )) 
+
+
+    }
+
+    </div>
+    ) : <p className="no-image">No images found</p>
+}
+
+        </div>
+    )
+}
+
+export default Gallery
